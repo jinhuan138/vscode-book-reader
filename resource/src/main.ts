07c4732d80ebb4b47dea2c27a6be0e9ec67a145e
lindacong <2796009827@qq.com>
2024-06-07 16:31:26 +0800

    git commit -m "init"

---
+++ resource/src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from 'vue'
+import 'element-plus/dist/index.css'
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import './app.css'
+//https://www.npmjs.com/package/ebook-convert
+
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+app.use(ElementPlus)
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+// https://cn.vitejs.dev/
+export default defineConfig({
+	plugins: [vue(),viteSingleFile()],
+	server: {
+		port: 8025,
+	},
+	root:"./resource",
+	base:"./resource"
+});
