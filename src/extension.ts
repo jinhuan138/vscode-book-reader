9379a413e54460533a40d1393411f1c4d7d67741
lindacong <2796009827@qq.com>
2024-06-14 09:16:54 +0800

    feat: add info&setting

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,8 @@ import { BookViewerProvider } from "./bookViewerProvider";
 //https://rackar.github.io/vscode-ext-doccn
 //https://code.visualstudio.com/api
 //https://juejin.cn/post/7208370120850079799#heading-31
+//https://github.com/aooiuu/any-reader.git
 export function activate(context: vscode.ExtensionContext) {
-	console.log('激活')
 	const option = {
 		webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true },
 	};
