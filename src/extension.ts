d94331327a32f2795e0efc2a25bfcc2e15cb6994
lingdacong <2796009827@qq.com>
2024-06-28 13:37:51 +0800

    feat: add SidebarViewerProvider

--- src/extension.ts
+++ src/extension.ts
@@ -1,17 +1,23 @@
-import * as vscode from "vscode";
-import { BookViewerProvider } from "./bookViewerProvider";
+import * as vscode from 'vscode'
+import { BookViewerProvider } from './bookViewerProvider'
+import { SidebarViewerProvider } from './sidebarViewerProvider'
 
 //https://rackar.github.io/vscode-ext-doccn
 //https://code.visualstudio.com/api
 //https://juejin.cn/post/7208370120850079799#heading-31
 //https://github.com/aooiuu/any-reader.git
 export function activate(context: vscode.ExtensionContext) {
-	const option = {
-		webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true },
-	};
-	vscode.window.registerCustomEditorProvider(
-		"bookReader",
-		new BookViewerProvider(context),
-		option
-	);
+  const option = {
+    webviewOptions: { retainContextWhenHidden: true, enableFindWidget: true },
+  }
+  vscode.window.registerCustomEditorProvider(
+    'bookReader',
+    new BookViewerProvider(context),
+    option,
+  )
+  vscode.window.registerWebviewViewProvider(
+    'book-reader-webview',
+    new SidebarViewerProvider(context),
+    option,
+  )
 }
