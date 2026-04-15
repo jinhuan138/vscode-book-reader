a9998d20516ff4b881b2b34654c8179225873e04
lindacong <2796009827@qq.com>
2026-04-15 17:45:56 +0800

    fix: bubble menu

--- resource/src/components/panel/BubbleMenu.vue
+++ resource/src/components/panel/BubbleMenu.vue
@@ -1,29 +1,34 @@
 <template>
-  <el-popover v-model:visible="isVisible" popper-class="bubble">
+  <el-popover :visible="isVisible" popper-class="bubble" :width="170">
     <template #reference>
       <span ref="popRef" style="position: absolute; visibility: hidden">{{ text }}</span>
     </template>
     <el-button-group>
-      <el-button :icon="Brush" @click="onHLBtn"></el-button>
+      <el-button :icon="Brush" @click="onHLBtn" v-if="!editAnnotation"></el-button>
+      <el-button :icon="Delete" @click="removeAnnotation" v-else></el-button>
       <el-button :icon="CopyDocument" @click="copyText"></el-button>
-      <el-popover width="200" trigger="hover">
-        <template #reference>
-          <el-button :icon="Collection"></el-button>
-        </template>
-        <div class="el-popover__title">
-          <el-select v-model="translateTo" placeholder="translateTo" style="width: 240px">
-            <el-option v-for="(label, code) in lang" :key="code" :label="label" :value="code" />
-          </el-select>
-        </div>
-        {{ translatedText }}
-      </el-popover>
+      <el-button :icon="Collection" ref="translatePop"></el-button>
     </el-button-group>
+    <el-popover width="200" trigger="hover" :virtual-ref="translatePop">
+      <div class="el-popover__title">
+        <el-select v-model="translateTo" placeholder="translateTo" style="width: 200px" size="small">
+          <el-option v-for="(label, code) in lang" :key="code" :label="label" :value="code" />
+        </el-select>
+      </div>
+      {{ translatedText }}
+    </el-popover>
+    <div class="color-option-container">
+      <div class="color-option" v-for="color in colorOption"
+        :style="{ backgroundColor: color, border: color === highlightColor ? '2px solid rgba(75, 75, 75, 1)' : 'none' }"
+        @click="highlightColor = color">
+      </div>
+    </div>
   </el-popover>
 </template>
 
 <script setup lang="ts">
 import { useClipboard, useTextSelection } from '@vueuse/core'
-import { Brush, CopyDocument, Collection } from '@element-plus/icons-vue'
+import { Brush, Delete, CopyDocument, Collection } from '@element-plus/icons-vue'
 import { ref } from 'vue'
 import { ElMessage } from 'element-plus'
 import 'element-plus/es/components/message/style/css'
@@ -34,11 +39,14 @@ import useVscode from '@/hooks/useVscode'
 const { bookInfo } = useStore()
 const vscode = useVscode()
 const text = ref('')
+const colorOption = ["#FBF1D1", "#EFEEB0", "#CAEFC9", "#76BEE9"]
+const highlightColor = ref('#FBF1D1')
 
 const isVisible = ref(false)
 const cfiRange = ref<Range[] | null>(null)
 let currentIndex = 0
 const popRef = ref<null | HTMLElement>(null)
+const translatePop = ref<null | HTMLElement>(null)
 const { copy } = useClipboard({ source: text })
 
 const init = (win: Window) => {
@@ -64,9 +72,12 @@ if (isEpub()) {
       '-ms-user-select': 'none' /* Internet Explorer/Edge */,
       'user-select': 'none',
     },
+    '::selection': {
+      cursor: 'pointer'
+    }
   })
   bookInfo.value!.highlights?.forEach((annotation) => {
-    rendition.value.annotations.highlight(annotation.value)
+    addAnnotation(annotation)
   })
   rendition.value.on('rendered', (e: Event, iframe: any) => {
     const win = iframe?.iframe?.contentWindow
@@ -74,7 +85,7 @@ if (isEpub()) {
   })
 } else {
   bookInfo.value!.highlights?.forEach((annotation) => {
-    rendition.value.addAnnotation(annotation)
+    addAnnotation(annotation)
   })
   rendition.value.addEventListener('load', (e) => {
     const { doc, index } = e.detail
@@ -89,6 +100,12 @@ if (isEpub()) {
     else if (type === 'underline') draw(Overlayer.underline, { color })
     else if (type === 'squiggly') draw(Overlayer.squiggly, { color })
   })
+  rendition.value.addEventListener("show-annotation", (e) => {
+    console.log("show-annotation");
+    // const annotation = this.annotationsByValue.get(e.detail.value);
+    // const pos = getPosition(e.detail.range);
+    // onAnnotationClick({ annotation, pos });
+  });
 }
 const copyText = () => {
   copy(text.value).then(() => {
@@ -105,7 +122,7 @@ const setProps = (react: DOMRect, cfiRangeValue: Range[]) => {
     : rendition.value.renderer.getBoundingClientRect()
   const reference = popRef.value
 
-  reference!.style.left = `${react.x + viewRect.x - (rendition.value.manager.scrollLeft || 0)}px`
+  reference!.style.left = `${react.x + viewRect.x - (rendition.value.manager?.scrollLeft || 0)}px`
   reference!.style.top = `${react.y + viewRect.y}px`
   reference!.style.width = react.width + 'px'
   reference!.style.height = react.height + 'px'
@@ -119,30 +136,62 @@ const hide = () => {
   translatedText.value = 'No Data'
   cfiRange.value = null
 }
+
+const editAnnotation = ref(null)
+const highlightClick = (annotation, react) => {
+  console.log(react.width)
+  console.log(react.y)
+  editAnnotation.value = annotation
+  const { note } = annotation
+  text.value = note
+  const viewRect = isEpub()
+    ? rendition.value.manager.container.getBoundingClientRect()
+    : rendition.value.renderer.getBoundingClientRect()
+  const reference = popRef.value
+  reference!.style.left = `${react.x + viewRect.x - (rendition.value.manager?.scrollLeft || 0)}px`
+  reference!.style.top = `${react.y + viewRect.y}px`
+  reference!.style.width = react.width + 'px'
+  reference!.style.height = react.height + 'px'
+  isVisible.value = true
+}
+function addAnnotation(annotation) {
+  if (isEpub()) {
+    const { color, value } = annotation
+    rendition.value.annotations.add("highlight", value, {}, (e: MouseEvent) => highlightClick(annotation, (e.target as HTMLElement).getBoundingClientRect()), "hl", { "fill": color, "fill-opacity": "0.3", "mix-blend-mode": "multiply" })
+  } else {
+    rendition.value.addAnnotation(annotation)
+  }
+}
+
+function removeAnnotation() {
+  if (isEpub()) {
+    const { value } = editAnnotation.value!
+    rendition.value.annotations.remove(value, 'highlight')
+    bookInfo.value!.highlights = bookInfo.value!.highlights.filter(i => i.value == value)
+    hide()
+  } else {
+    // rendition.value.addAnnotation(annotation)
+  }
+}
 const onHLBtn = () => {
   if (!bookInfo.value!.highlights) {
     bookInfo.value!.highlights = []
   }
+  let cfi = ''
   if (isEpub()) {
     const [contents] = rendition.value.getContents()
-    const cfi = contents.cfiFromRange(cfiRange.value![0])
-    const annotation = {
-      value: cfi,
-      note: text.value,
-    }
-    rendition.value.annotations.highlight(cfi)
-    bookInfo.value!.highlights.push(annotation)
+    cfi = contents.cfiFromRange(cfiRange.value![0])
   } else {
-    const cfi = rendition.value.getCFI(currentIndex, cfiRange.value)
-    const annotation = {
-      value: cfi,
-      type: 'highlight',
-      color: 'red',
-      note: text.value,
-    }
-    rendition.value.addAnnotation(annotation)
-    bookInfo.value!.highlights.push(annotation)
+    cfi = rendition.value.getCFI(currentIndex, cfiRange.value)
   }
+  const annotation = {
+    value: cfi,
+    type: 'highlight',
+    note: text.value,
+    color: highlightColor.value,
+  }
+  addAnnotation(annotation)
+  bookInfo.value!.highlights.push(annotation)
 }
 
 const translateTo = ref('en')
@@ -302,6 +351,51 @@ window.addEventListener('message', ({ data }) => {
 
 <style lang="scss" scoped>
 .bubble {
+  width: 150px;
   padding: 0px;
+
+  @keyframes slide-right {
+    0% {
+      transform: translateX(30px);
+      opacity: 0;
+    }
+
+    100% {
+      transform: translateX(0px);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-left {
+    0% {
+      transform: translateX(-50px);
+      opacity: 0;
+    }
+
+    100% {
+      transform: translateX(0px);
+      opacity: 1;
+    }
+  }
+
+  .color-option-container {
+    width: 90%;
+    height: 25px;
+    margin: 5px 0;
+    padding-bottom: 5px;
+    flex-shrink: 0;
+    display: flex;
+    justify-content: space-around;
+
+    .color-option {
+      width: 25px;
+      height: 25px;
+      border-radius: 50%;
+      opacity: 1;
+      box-sizing: border-box;
+      cursor: pointer;
+      animation: slide-left 0.2s ease-in-out 0s 1;
+    }
+  }
 }
 </style>
